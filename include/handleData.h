2477e9c9991e3a0be2a3ee829f76c0d80e3afa85
zAlexisD <145620218+zAlexisD@users.noreply.github.com>
2026-06-24 12:43:35 +0900

    Sensor Commit 1.0.0 : Sample code to test ADXL345 working

---
+++ include/handleData.h
@@ -0,0 +1,13 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+/* Define accelerometer and assign ID to it */
+Adafruit_ADXL345_Unified adxl345 = Adafruit_ADXL345_Unified(12345);
+
+/* Setup and Test functions prototype*/
+void displaySensorDetails(Adafruit_ADXL345_Unified accel);
+void displayDataRate(Adafruit_ADXL345_Unified accel);
+void displayRange(Adafruit_ADXL345_Unified accel);
+void setupSensor(Adafruit_ADXL345_Unified accel);
+void sensorLoop(Adafruit_ADXL345_Unified accel);
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
-#include <Arduino.h>
+#include "handleData.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(void) 
+{
+  Serial.begin(115200);
+  /* Set and display header for ADXL345 sensor */
+  setupSensor(adxl345);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(void) 
+{
+  /* Loop sensor values reading */
+  sensorLoop(adxl345);
 }
